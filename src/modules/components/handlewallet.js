3d93956c414c5b46c282db7db3954b236cefddc7
Navtej Dhillon <navtejxs@gmail.com>
2022-08-25 00:39:07 +1200

    Fixing Wallet

--- src/modules/components/handlewallet.js
+++ src/modules/components/handlewallet.js
@@ -1,30 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Button, Box } from "@mui/material";
-import { useMetaMask } from "ethers-react";
 import {networks} from '../../utils/networks'
-import {checkNetwork} from '../../utils/functions'
+import {checkNetwork,checkIfWalletIsConnected,connectWallet} from '../../utils/functions'
+
 const rightLink = {
   fontSize: 16,
   color: "common.white",
   ml: 3,
 };
 export const HandleWallet = () => {
-  const {isConnected,connectWallet} = useMetaMask()
+  const {isConnected,setIsConnectWallet} = useState(false)
   const [network,setNetwork]=useState();
   const {ethereum}=window
   useEffect(()=>{
     if(ethereum){(async()=>{
-        
-        try{
-          let chain=  await checkNetwork();
-          setNetwork(networks[chain])
+        const wllt=await checkIfWalletIsConnected();
+        if(wllt)
+        {
+          setIsConnectWallet(true)
+          getNetwork()
         }
-        catch(error){
-            console.log(error);
+        else{
+          console.log("not connected")
         }
     })();
 }
 },[ethereum])
+const getNetwork=async()=>{
+  try{
+    let chain= await checkNetwork();
+    setNetwork(networks[chain])
+  }
+  catch(error){
+      console.log(error);
+  }
+}
 console.log(isConnected)
   return (
     <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -24,3 +24,16 @@ export const checkNetwork = async () => {
     const chainId = await ethereum.request({ method: "eth_chainId" });
     return chainId
 };
+export const connectWallet = async () => {
+    try {
+        const { ethereum } = window;
+        if (!ethereum) {
+            alert("Get MetaMask -> https://metamask.io/");
+            return;
+        }
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        console.log("Connected", accounts[0]);
+    } catch (error) {
+        console.log(error);
+    }
+};
